06616af566f20f38e1814f21d10544a75351296d
pyagni <bhattacharyya.agni@gmail.com>
2020-06-24 03:44:38 +0530

    Date formatted

--- app/serializers.py
+++ app/serializers.py
@@ -4,6 +4,9 @@ from .models import User, Activity
 
 
 class ActivitySerializerHelper(serializers.ModelSerializer):
+	start_time = serializers.DateTimeField(format='%b %d %Y  %H:%M%p')
+	end_time = serializers.DateTimeField(format="%b %d %Y  %H:%M%p")
+
 	class Meta:
 		model = Activity
 		fields = ['start_time', 'end_time']
@@ -16,6 +19,8 @@ class UserSerializer(serializers.ModelSerializer):
 		fields = ['user_id', 'real_name', 'tz', 'activities']
 
 class ActivitySerializer(serializers.ModelSerializer):
+	start_time = serializers.DateTimeField(format='%b %d %Y  %H:%M%p')
+	end_time = serializers.DateTimeField(format="%b %d %Y  %H:%M%p")
 	class Meta:
 		model = Activity
 		fields = ['user','start_time','end_time']
